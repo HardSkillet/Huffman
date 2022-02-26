d481fc21678574406ba301536f20d81b4a3ccd5f
HardSkillet <43767465+HardSkillet@users.noreply.github.com>
2022-02-26 13:11:18 +0300

    Добавьте файлы проекта.

---
+++ Generator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HuffmanTest
+{
+    public class Generator
+    {
+        static private int step = 0;
+        const int n = 100;
+        static StreamReader sr;
+        static string str;
+        static public string ParsingText()
+        {
+            if (str.Length - n * step >= n)
+            {
+                string ans = null;
+                for (int i = n * step; i < n * (step + 1); i++)
+                {
+                    ans += str[i];
+                }
+                step += 1;
+                return ans;
+            }
+            else {
+                return null;
+            }
+        }
+        public Generator(string path)
+        {
+            try
+            {
+                using (sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        str = sr.ReadToEnd();
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using HuffmanTest;
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double i = 0;
+            long sum = 0;
+            while (true)
+            {
+                Generator temp = new Generator("Tom1.txt");
+
+                string input = Generator.ParsingText();
+
+                if (input != null)
+                {
+                    Stopwatch stopWatch = new Stopwatch();
+                    HuffmanTree huffmanTree = new HuffmanTree();
+                    stopWatch.Start();
+
+                    huffmanTree.Build(input);
+
+                    stopWatch.Stop();
+                    long ts = stopWatch.ElapsedTicks;
+                    i++;
+                    sum += ts;
+                    Console.WriteLine("RunTime " + ts);
+                }
+                else break;
+
+            }
+            Console.WriteLine("end " + (sum / i));
+            Console.ReadLine();
+        }
+
+    }
+}
